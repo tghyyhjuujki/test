965ed5f69ff6db8bb18525aae2620a47c806bc78
tghyyhjuujki <tghyyhju@gmail.com>
2020-08-28 17:16:22 +0900

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
-      <PostListTitle title="All Posts" />
+      <PostListTitle title="Posts" />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
