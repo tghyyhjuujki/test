12439e13b69f18ef90a0cdf315c7d9ae05c4dedf
tghyyhjuujki <tghyyhju@gmail.com>
2020-08-28 09:30:00 +0900

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,10 +10,10 @@ const AboutPage = ({ location, data }) => (
     <h1>About Me</h1>
     <p>github : <a href="http://github.com/tghyyhjuujki" target="blank">github.com/tghyyhjuujki</a></p>
 
-    <p>비공식 연습용 블로그입니다.</p>
+    <p>비영리 블로그입니다.</p>
     <p>사이트를 구축하는데에 다음 자료들을 참고했습니다</p>
     <p>start-kit : <a href="https://www.gatsbyjs.org/starters/gatsbyjs/gatsby-starter-blog/" target="blank">gatsby-start-blog</a></p>
-    <p>Design and Category: <a href="https://ohseunghyeon.github.io/" target="blank">Lazy Nyaong</a></p>
+    <p>Design and Category: <a href="https://github.com/ohseunghyeon/gatsby-blog" target="blank">https://github.com/ohseunghyeon/gatsby-blog</a></p>
   </Layout>
 )
 
