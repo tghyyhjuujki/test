43e00139d563f2e77bdc5e36acea59a6ed021546
tghyyhjuujki <tghyyhju@gmail.com>
2020-08-28 09:14:26 +0900

    update

--- aws/access_key.md
+++
@@ -1,1 +0,0 @@
-https://edu24-edowon-mybucket.s3.ap-northeast-2.amazonaws.com/access-key/new_user_credentials_20200811.csv
--- aws/nodejs_app/config/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  host     : '[rds host]',
-  user     : '[rds user]',
-  password : '[rds password]',
-  database : '[rds database]'
-}
